f511007e08fbab2974a970f275fd2c9dcd847b7f
Suthichanon Poonkrankay <suthichanon@gmail.com>
2023-02-05 19:29:20 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,4 @@
 <template>
-  <nav class="nav">
-    <div class="container">
-      <div class="logo">
-        <a href="#">Your Logo</a>
-      </div>
-      <div id="mainListDiv" class="main_list">
-        <ul class="navlinks">
-          <li><a href="">About</a></li>
-          <li><a href="#">Portfolio</a></li>
-          <li><a href="#">Services</a></li>
-          <li><a href="#">Contact</a></li>
-        </ul>
-      </div>
-      <span class="navTrigger">
-        <i></i>
-        <i></i>
-        <i></i>
-      </span>
-    </div>
-  </nav>
-  <section class="home">
-  </section>
-  <div style="height: 1000px">
-    <section class="input">
-    <input type="text">
-  </section>
-  </div>
-  
-  
 
 </template>
 <script lang="ts">
